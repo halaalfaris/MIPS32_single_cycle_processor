79a3146867b9d2475197b084ff6af519864e956b
halaalfaris <114068703+halaalfaris@users.noreply.github.com>
2023-12-07 23:21:45 +0300

    Update mips32_tb.v

--- mips32_tb.v
+++ mips32_tb.v
@@ -1,11 +1,17 @@
 `timescale 1 ns/1 ns
+`include "mip32.v"
+
 module mips32_tb ();
 
 reg clk, reset;
 
-mip32_b mip (clk, reset);
+mip32 mip (clk, reset);
+
+
 
 initial begin 
+	$dumpfile("Benchmark1.vcd");      // create a VCD waveform dump called "wave.vcd"
+    $dumpvars(0, mip);
 	reset = 1'b1;
 	clk = 1'b0;
 	#5; 
@@ -14,11 +20,13 @@ initial begin
 	reset = 1'b0;
 	#3;
 	
-	
 	forever begin 
 	clk = #5 ~clk;
 	end
-	
+
+	#1000;
+    $finish;
+
 	end
 
 
