51eb2c9ff9ebf7fbee46e4f2f636e5b76b9d6bc8
halaalfaris <114068703+halaalfaris@users.noreply.github.com>
2023-12-14 20:44:40 +0300

    Update IDEX.v

--- IDEX.v
+++ IDEX.v
@@ -1,10 +1,10 @@
 module IDEX(clock, reset, iPC, iIR, iwrite_addr, ibranch, ijump, imem_read, imem_to_reg, ipc_to_reg, ialuop, imem_write, ialusrc, ireg_write, iread_data1, iread_data2, isign_ext,
-oPC, oIR, owrite_addr, obranch, ojump, omem_read, opc_to_reg, oaluop, omem_write, oalusrc, oreg_write, oread_data1, oread_data2, osign_ext);
+oPC, oIR, owrite_addr, omem_read, opc_to_reg, oaluop, omem_write, oalusrc, oreg_write, oread_data1, oread_data2, osign_ext);
 input clock, reset, ibranch, ijump, imem_read, imem_to_reg, ipc_to_reg, imem_write, ialusrc, ireg_write;
 input [31:0] iPC, iIR, iread_data1, iread_data2, isign_ext;
 input[4:0] iwrite_addr;
 input [3:0] ialuop;
-output reg obranch, ojump, omem_read, omem_to_reg, opc_to_reg, omem_write, oalusrc, oreg_write;
+output reg  omem_read, omem_to_reg, opc_to_reg, omem_write, oalusrc, oreg_write;
 output reg [31:0] oPC, oIR, oread_data1, oread_data2, osign_ext;
 output reg [4:0] owrite_addr;
 output reg [3:0] oaluop;
@@ -16,8 +16,7 @@ end
 always@ (posedge clock) 
 begin
 	if(~reset) begin
-		obranch <= ibranch;
-		ojump <= ijump;
+
 		omem_read <= imem_read;
 		omem_to_reg <= imem_to_reg;
 		opc_to_reg <= ipc_to_reg;
