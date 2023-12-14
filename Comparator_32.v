2f2eab1ca5a1b45b3742ed0e498e5339cb3b461e
halaalfaris <114068703+halaalfaris@users.noreply.github.com>
2023-12-14 20:12:41 +0300

    Update Comparator_32.v

--- Comparator_32.v
+++ Comparator_32.v
@@ -1,45 +1,26 @@
 module Comparator_32bit (
   input wire [31:0] A,
   input wire [31:0] B,
+  input wire [31:0] IR,
   input wire clk,
   input wire rst,
-  output wire eq,
-  output wire lt,
-  output wire gt,
-  output wire ge,
-  output wire le
+  output wire branchYes
 );
-
+  wire [5:0]opcode= [31:26]IR;
   reg eq, lt, gt, ge, le;
 
   always @(posedge clk or posedge rst) begin
-    if (rst) begin
-      eq <= 1'b0;
-      lt <= 1'b0;
-      gt <= 1'b0;
-      ge <= 1'b0;
-      le <= 1'b0;
-    end else begin
-      if (A == B) begin
-        eq <= 1'b1;
-        lt <= 1'b0;
-        gt <= 1'b0;
-        le <= 1'b1;
-        ge <= 1'b1;
-      end else if (A < B) begin
-        eq <= 1'b0;
-        lt <= 1'b1;
-        gt <= 1'b0;
-        le <= 1'b1;
-        ge <= 1'b0;
-      end else begin
-        eq <= 1'b0;
-        lt <= 1'b0;
-        gt <= 1'b1;
-        le <= 1'b0;
-        ge <= 1'b1;
+    if (rst == 1) branch <=0;
+    else begin
+    case(opcode) begin
+        6'h8: begin if(In1 == In2) branchYes <= 1; else branchYes<= 0; res <= In1 - In2; end
+        6'h9: begin if(In1 != In2) branchYes <= 1; else branchYes<= 0; res <= In1 - In2; end
+        6'hA: begin if(In1 >= In2) branchYes <= 1; else branchYes<= 0; res <= In1 - In2; end
+        6'hB: begin if(In1 > In2) branchYes <= 1; else branchYes<= 0;  res <= In1 - In2;  end
+        6'hC: begin if(In1 <= In2) branchYes <= 1; else branchYes<= 0; res <= In1 - In2; end
+        6'hD: begin if(In1 < In2) branchYes <= 1; else branchYes<= 0;  res <= In1 - In2;  end
+      endcase
       end
-    end
   end
 
 endmodule
