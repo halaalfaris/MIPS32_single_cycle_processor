eda64b1de3bad9019b67d124005e47de33fd925a
halaalfaris <114068703+halaalfaris@users.noreply.github.com>
2023-12-24 22:39:26 +0300

    Update mux_4to1.v

--- mux_4to1.v
+++ mux_4to1.v
@@ -4,16 +4,16 @@ module mux_4to1 (
   input wire [31:0] data_input_2,
   input wire [31:0] data_input_3,
   input wire [1:0] select,
-  output reg [31:0] output
+  output reg [31:0] data_output
 );
 
 always @* begin
   case (select)
-    2'b00: output = data_input_0;
-    2'b01: output = data_input_1;
-    2'b10: output = data_input_2;
-    2'b11: output = data_input_3;
-    default: output = 32'b0; // Default case
+    2'b00: data_output = data_input_0;
+    2'b01: data_output = data_input_1;
+    2'b10: data_output = data_input_2;
+    2'b11: data_output = data_input_3;
+    default: data_output = 32'b0; // Default case
   endcase
 end
 
