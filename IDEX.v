86cd5b03f52e38716e5cf726d3f95549853707aa
halaalfaris <114068703+halaalfaris@users.noreply.github.com>
2023-12-24 22:07:41 +0300

    Update IDEX.v
    I added rs1 and rs2 adresses to the register because we will need them in the forwarding unit

--- IDEX.v
+++ IDEX.v
@@ -1,12 +1,12 @@
 module IDEX(clock, reset, iPC, iIR, iwrite_addr, ibranch, ijump, imem_read, imem_to_reg, ipc_to_reg, ialuop, imem_write, ialusrc, ireg_write, iread_data1, iread_data2, isign_ext,
 oPC, oIR, owrite_addr, omem_read, opc_to_reg, oaluop, omem_write, oalusrc, oreg_write, oread_data1, oread_data2, osign_ext, hazard_detected);
-input clock, reset, ibranch, ijump, imem_read, imem_to_reg, ipc_to_reg, imem_write, ialusrc, ireg_write;
+input clock, reset, ibranch, ijump, imem_read, imem_to_reg, ipc_to_reg, imem_write, ialusrc, ireg_write,iRS1,iRS2,oRS1,oRS2;
 input [31:0] iPC, iIR, iread_data1, iread_data2, isign_ext;
 input[4:0] iwrite_addr;
-input [3:0] ialuop;
+input [3:0] ialuop,iRS1,iRS2;
 output reg  omem_read, omem_to_reg, opc_to_reg, omem_write, oalusrc, oreg_write;
 output reg [31:0] oPC, oIR, oread_data1, oread_data2, osign_ext;
-output reg [4:0] owrite_addr;
+output reg [4:0] owrite_addr,oRS1,oRS2;
 output reg [3:0] oaluop;
 wire flush;
 assign flush = reset || hazard_detected;
@@ -32,20 +32,24 @@ begin
 		owrite_addr <= iwrite_addr;
 		oaluop <= ialuop;
 		osign_ext <= isign_ext;
+		oRS1 <=iRS1;
+		oRS2 <=iRS2;
 	end
 	else begin
 		omem_read <= 0;
 		omem_to_reg <= 0;
 		opc_to_reg <= 0;
 		omem_write <= 0;
-		oalusrc <= ialusrc;
+		oalusrc <= 0;
 		oreg_write <= 0;
+		oRS1 <=0;
+		oRS2 <=0;
 		oPC <= iPC;
 		oIR <= iIR;
-		oread_data1 <= iread_data1;
-		oread_data2 <= iread_data2;
-		owrite_addr <= iwrite_addr;
-		oaluop <= ialuop;
-		osign_ext <= isign_ext;
+		oread_data1 <= 0;
+		oread_data2 <= 0;
+		owrite_addr <= 0;
+		oaluop <=0;
+		osign_ext <= 0;
 end
 endmodule
